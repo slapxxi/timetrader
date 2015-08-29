0f22171a86c8df8256c6767689a03b5b562f65be
Slava Pavlutin <sl.pavlutin@gmail.com>
2015-08-29 14:21:34 +0300

    Delete unnecessary files

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls import url
 
-from pages.views import youtube
-
 
 urlpatterns = [
-  url(r'^youtube/', youtube),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,1 @@
 from django.shortcuts import render
-
-
-def youtube(request):
-  return render(request, template_name='youtube.html')
