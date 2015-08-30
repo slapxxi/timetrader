bae7990dc47e8305a834e215d0b5867a09f28009
Slava Pavlutin <sl.pavlutin@gmail.com>
2015-08-30 16:15:23 +0300

    Delete 'lists' application

--- timetrader/urls.py
+++ timetrader/urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 import pages.urls
-import lists.urls
 
 
 urlpatterns = [
-  url(r'^lists', include(lists.urls)),
   url(r'', include(pages.urls)),
   # url(r'^admin/', include(admin.site.urls)),
 ]
